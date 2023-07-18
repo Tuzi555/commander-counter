06f9364a9f2f0e4a1c83f67017d49c6cc61f68dc
Tuzar Jakub <jtuzar@csas.cz>
2023-07-18 10:53:09 +0200

    initial commit and project preparation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 col-span-6">1</div>
+      <div className="bg-green-500 col-span-6">2</div>
+      <div className="bg-blue-500 col-span-6">3</div>
+      <div className="bg-yellow-500 col-span-6">4</div>
+    </>
+  );
+}
+
+export default App;
